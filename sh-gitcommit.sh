7a8c4c9f6b720bee1cbe09a1eb46efe8829d1d97
Doddi Priyambodo <doddi@bicarait.com>
2023-07-16 23:30:11 +0700

    Doddi Priyambodo is committing PUSH

--- sh-gitcommit.sh
+++ sh-gitcommit.sh
@@ -1,3 +1,6 @@
 #/bin/bash
 git config --global user.name "Doddi Priyambodo"
 git config --global user.email "doddi@bicarait.com"
+git add .
+git commit -m "Doddi Priyambodo is committing PUSH"
+git push
